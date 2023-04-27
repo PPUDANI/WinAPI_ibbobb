160228f3ca80bfb92f59683f579f9502231e2f99
조대규 <zxvk1030@naver.com>
2023-04-27 20:50:52 +0900

    20203/04/27

--- WinAPI_ibbobb/GameEngineBase/GameEngineString.cpp
+++ WinAPI_ibbobb/GameEngineBase/GameEngineString.cpp
@@ -1,11 +1,10 @@
 #include "GameEngineString.h"
 
-GameEngineString::GameEngineString()
+GameEngineString::GameEngineString() 
 {
-
 }
 
-GameEngineString::~GameEngineString()
+GameEngineString::~GameEngineString() 
 {
-
 }
+
